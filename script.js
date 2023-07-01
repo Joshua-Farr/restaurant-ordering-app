8da2c839480f9f62e25ed045d249a4abbc5536b0
Joshua Farr <94776350+Joshua-Farr@users.noreply.github.com>
2023-07-01 15:40:50 -0500

    Updated styling & menu rendering via JavaScript

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 import { menuArray } from "./data.js"
 
 const menu = document.getElementById("menu");
+let order = [];
+let orderValue = 0;
 
 
 
 function renderMenu(){
     menuArray.forEach(function(item){
-        console.log(item);
+        menu.innerHTML +=
+        `<li class="menu-item" id="menu-item">
+            <h2>${item.emoji}</h2>
+            <div class="item">
+                <h3 class="item-title" id ="item-title">${item.name}</h3>
+                <p class="item-description" id="item-description">${item.ingredients}</p>
+                <h3 class="item-price" id="item-price">$${item.price}</h3>
+            </div>
+            <button class="select-button" id="select-button">+</button>
+        </li>`;
     })
 
 }
+
+
+
 
 renderMenu();
